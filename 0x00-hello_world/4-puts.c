81f92c7c54fdfea48765cf2d5d5876eb1b7dc9da
mohabdona <eltabeei.mohab@gmail.com>
2023-03-16 10:29:41 -0700

    Task0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#!/bin/bash
-#include <stdio.h>
-/*
- * main - print a line of code using puts
+#include<stdio.h>
+/**
+ * main - Entry point
  *
- * Return:0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
